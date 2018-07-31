6f25fca69ff09ef63a06a72e41107ddac2ee2577
Austin Soplata <austin.soplata@gmail.com>
2018-07-31 13:04:51 -0400

    Fix depr stats, only thing left is syn diffs Changes to be committed: 	modified:   models/iAMPAcomb_INdr_PYso.mech 	modified:   models/iAMPAcomb_PYdr_PYso.mech 	modified:   models/iGABAAcomb_PYdr_INso.mech 	modified:   models/iLeak_INdr.mech 	modified:   models/iNMDA_INdr_PYso.mech 	modified:   models/iNMDA_PYdr_PYso.mech 	modified:   models/iNa_INdr.mech 	modified:   models/newReleaseUpdate.m

--- models/newReleaseUpdate.m
+++ models/newReleaseUpdate.m
@@ -3,14 +3,14 @@ function output = newReleaseUpdate(timeSinceSpike, miniFreq, epsilon, N_pre)
 %
 % This function is how we calculate new values of the state variable
 % `newRelease` for the 'Mini'-type synaptic mechanisms in the DynaSim
-% implementation of (Bazhenov et al., 2002). This is similar to the original
+% implementation of (Krishnan et al., 2016). This is similar to the original
 % code in "currents.cpp" lines 520-524. This needs to be in an external
 % function for simplification of the many checks required for this.
 %
 % - References:
-%     - Bazhenov M, Timofeev I, Steriade M, Sejnowski TJ. Model of thalamocortical
-%         slow-wave sleep oscillations and transitions to activated states. The
-%         Journal of Neuroscience. 2002;22: 8691–8704.
+%     - Krishnan GP, Chauvette S, Shamie I, Soltani S, Timofeev I, Cash SS, et
+%         al. Cellular and neurochemical basis of sleep stages in the
+%         thalamocortical network. eLife. 2016;5: e18607
 
 % This corresponds to the `S` in the original code, in "currents.cpp" line 521.
 S = rand(1,N_pre);
@@ -25,10 +25,15 @@ end
 %     when timeSinceSpike is small or zero, this causes our `output` to be Inf,
 %     divided by zero, or astronomically large, which turns our values into
 %     NaNs silently, which completely (and silently) breaks the simulation!
-timeDifference = timeSinceSpike + (timeSinceSpike < 100).*100;
+% AES
+% timeDifference = timeSinceSpike + (timeSinceSpike < 100).*100;
+timeDifference = timeSinceSpike + (timeSinceSpike < 70).*70;
 
 % The denominator here corresponds to the `SS` in the original code, in
 %     "currents.cpp" line 520.
-output = -log(S)./((2.0./(1.0+exp(-timeDifference./miniFreq))-1.0)./250.0);
+% AES
+% output = -log(S)./((2.0./(1.0+exp(-timeDifference./miniFreq))-1.0)./250.0);
+tau = 50
+output = -log(S)./(log((timeDifference + tau)./tau)./400.0);
 
 end
